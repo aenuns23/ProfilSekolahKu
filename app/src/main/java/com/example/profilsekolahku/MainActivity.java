dd4d107926682de4f43cdbaa812ec86d442e18a9
aenuns23 <aenunsafitri6@gmail.com>
2022-02-22 14:20:30 +0700

    Profil sekolah

--- app/src/main/java/com/example/profilsekolahku/MainActivity.java
+++ app/src/main/java/com/example/profilsekolahku/MainActivity.java
@@ -1,14 +1,50 @@
 package com.example.profilsekolahku;
 
+
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+    }
+
+    public void profile(View view) {
+        Intent intent = new Intent(MainActivity.this, ProfilActivity.class);
+        startActivity(intent);
+    }
+
+    public void galeri(View view) {
+        Intent intent = new Intent(MainActivity.this, GaleriActivity.class);
+        startActivity(intent);
+    }
+
+    public void ekskul(View view) {
+        Intent intent = new Intent(MainActivity.this, EkskulActivity.class);
+        startActivity(intent);
+    }
+
+    public void perpus(View view) {
+        Intent intent = new Intent(MainActivity.this, PerpusActivity.class);
+        startActivity(intent);
+    }
+
+    public void guru(View view) {
+        Intent intent = new Intent(MainActivity.this, GuruActivity.class);
+        startActivity(intent);
+    }
+
+    public void fasilitas(View view) {
+        Intent intent = new Intent(MainActivity.this, FasillitasActivity.class);
+        startActivity(intent);
     }
 }
