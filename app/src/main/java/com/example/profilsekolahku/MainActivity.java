a6ba1f505f7ee29b791cc8161729715330769563
mang555 <akmal.fahrian346@gmail.com>
2022-02-24 13:58:14 +0700

    anggota 2

--- app/src/main/java/com/example/profilsekolahku/MainActivity.java
+++ app/src/main/java/com/example/profilsekolahku/MainActivity.java
@@ -7,14 +7,32 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
+import com.smarteist.autoimageslider.IndicatorView.animation.type.IndicatorAnimationType;
+import com.smarteist.autoimageslider.SliderAnimations;
+import com.smarteist.autoimageslider.SliderView;
+
 
 public class MainActivity extends AppCompatActivity {
+    SliderView sliderView;
+    int[] images = {R.drawable.ekskul1,
+            R.drawable.header,
+            R.drawable.profilsekolahsmp,
+    };
+
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        sliderView = findViewById(R.id.image_slider);
+
+        SliderAdapter sliderAdapter = new SliderAdapter(images);
+
+        sliderView.setSliderAdapter(sliderAdapter);
+        sliderView.setIndicatorAnimation(IndicatorAnimationType.WORM);
+        sliderView.setSliderTransformAnimation(SliderAnimations.DEPTHTRANSFORMATION);
+        sliderView.startAutoCycle();
 
     }
 
