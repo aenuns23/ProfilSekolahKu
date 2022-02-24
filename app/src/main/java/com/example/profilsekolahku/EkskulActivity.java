4738dd712c4a5a2a74d5d63c3f788e22f1751046
aenuns23 <aenunsafitri6@gmail.com>
2022-02-24 09:01:16 +0700

    Profil sekolahku

--- app/src/main/java/com/example/profilsekolahku/EkskulActivity.java
+++ app/src/main/java/com/example/profilsekolahku/EkskulActivity.java
@@ -3,6 +3,7 @@ package com.example.profilsekolahku;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class EkskulActivity extends AppCompatActivity {
 
@@ -10,5 +11,20 @@ public class EkskulActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ekskul);
+    }
+
+    public void osis(View view) {
+    }
+
+    public void pmr(View view) {
+    }
+
+    public void pramuka(View view) {
+    }
+
+    public void futsal(View view) {
+    }
+
+    public void bulutangkis(View view) {
     }
 }
